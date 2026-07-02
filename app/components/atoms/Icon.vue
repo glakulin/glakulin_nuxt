58f23b4744ee1019800080c01a6b931fc2e70d4b
glakulin <danielglakulin@gmail.com>
2026-07-02 23:26:50 +0400

    Icon component

---
+++ app/components/atoms/Icon.vue
@@ -0,0 +1,11 @@
+<!-- Компонент для иконок из nerd font -->
+
+<script setup lang="ts">
+const props = defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <i :class="`nf ${props.name}`"></i>
+</template>
