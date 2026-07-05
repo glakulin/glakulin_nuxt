a5fb502d5cbf385992b708fdeeff0a76415ab3ee
glakulin <danielglakulin@gmail.com>
2026-07-05 12:38:48 +0400

    Masonry компонент

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,7 @@
 <!-- Главная страница -->
 
 <script setup lang="ts">
-import { Flex, Icon, Text } from "~/components/atoms";
-import { Section } from "~/components/molecules";
-
 </script>
 
 <template>
-  <Section>
-    <template #heading><Icon name="nf-md-hand_wave" />Hello</template>
-    <Flex
-      direction="column" 
-    >
-      <Text family="heading" size="sm">I'm Glakulin</Text>
-    </Flex>
-  </Section>
 </template>
