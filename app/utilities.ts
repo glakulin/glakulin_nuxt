1cec7cd702cbefe34a283f9c129b31bdcdfe58eb
glakulin <danielglakulin@gmail.com>
2026-07-07 01:09:40 +0400

    новый хук, использовать ширину окна

--- app/utilities.ts
+++ app/utilities.ts
@@ -1,6 +1,6 @@
 // Полезные функции
 
-import { TOKENS, type Color_Name, type Color_Scheme, type Color_Shade } from "./tokens";
+import { TOKENS, type Color_Name, type Color_Scheme, type Color_Shade, type Size } from "./tokens";
 
 // Обработка чисел в css
 export type Number_Rem = number | readonly number[];
@@ -23,4 +23,21 @@ export const get_rem = (value: Number_Rem): string => {
 export const get_color = (scheme: Color_Scheme): string => {
   const [color_name, color_shade] = scheme.split("_") as [Color_Name, Color_Shade];
   return TOKENS.colors[color_name][color_shade];
+};
+
+
+// Текущий брейкпоинт по ширине экрана
+export const get_size = (width: number): Size => {
+  const screens = TOKENS.size.screen;
+  const sizes = Object.entries(screens) as [Size, number][];
+
+  let current: Size = "default";
+
+  for (const [size, breakpoint] of sizes) {
+    if (width >= breakpoint) {
+      current = size;
+    }
+  }
+
+  return current;
 };
