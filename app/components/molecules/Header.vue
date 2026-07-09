a4d697ece9e83077a2834679b50cc3e2368de186
glakulin <danielglakulin@gmail.com>
2026-07-10 01:12:50 +0400

    текущая страница

--- app/components/molecules/Header.vue
+++ app/components/molecules/Header.vue
@@ -6,6 +6,7 @@ import { Flex, Icon, Link, Text } from "../atoms";
 import { TOKENS } from "~/tokens";
 import { get_color, get_rem } from "~/utilities";
 import { HeaderMenu } from ".";
+import { PAGES } from "~/static";
 
 // Адаптив
 const { size } = use_window_size();
@@ -32,6 +33,7 @@ const css_rule = computed<Css_Rule>(() => ({
 
 // Текущая страница
 const route = useRoute();
+const current_page = PAGES.find(page => page.href === route.fullPath)
 
 // Состояние меню
 const is_menu_open = ref<boolean>(false);
@@ -41,7 +43,7 @@ const is_menu_open = ref<boolean>(false);
   <Flex tag="header" :css="css_rule_container" justify_content="center" :padding="[0, screen_padding]">
     <Flex :css="css_rule" :padding="12" justify_content="space-between" align_items="center">
       <Text family="body" size="sm"><svg :style="{aspectRatio: '97 / 24', height: '1em'}"><use href="/logo_full.svg"></use></svg></Text>
-      <Text family="body" size="sm" color="gray_8">{{ route.name }}</Text>
+      <Text family="body" size="sm" color="gray_8"><Flex :gap="4"><Icon :name="current_page?.icon" />{{ current_page?.label }}</Flex></Text>
       <Flex tag="button" @click="is_menu_open = !is_menu_open">
         <Text family="body" size="sm"><Link color="gray_1" color_hover="gray_3"><Icon :name="is_menu_open ? 'nf-md-close' : 'nf-md-menu'" /></Link></Text>
       </Flex>
